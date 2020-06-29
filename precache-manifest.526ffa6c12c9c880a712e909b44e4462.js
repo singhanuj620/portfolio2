461f6b6bf2b69ab368b08fbbbff4925c22261326
Anuj Singh <singhanuj620@gmail.com>
2020-06-29 11:04:48 +0530

    Updates

---
+++ precache-manifest.526ffa6c12c9c880a712e909b44e4462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3431113f2560c0d4eb10bc047313e358",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "d4b4e92780d4828bf230",
+    "url": "/port/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cb4c3dc96ab1a598675b",
+    "url": "/port/static/css/main.1b97bda9.chunk.css"
+  },
+  {
+    "revision": "d4b4e92780d4828bf230",
+    "url": "/port/static/js/2.4cc06c4a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/port/static/js/2.4cc06c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4c3dc96ab1a598675b",
+    "url": "/port/static/js/main.904ac947.chunk.js"
+  },
+  {
+    "revision": "30ff79d3aaa4d896cd61",
+    "url": "/port/static/js/runtime-main.dd51b5be.js"
+  }
+]);
